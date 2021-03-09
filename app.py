307ddd40e078c3780f16901a50d139f8be7231d7
Pitágoras Bandeira <pithagoras@hotmail.com>
2021-03-09 11:01:38 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,1 +1,45 @@
+##Version 2.0
+Version="2.0"
 
+import uuid
+import threading
+import os
+import sys
+import time
+#sys.path.append('C:/FIRMWARE/utils/lib')
+sys.path.append('/home/pi/pitagoras/devops/comgas/FIRMWARE/utils/lib')
+import vars
+import defs
+from azure.iot.device import IoTHubDeviceClient, Message, MethodResponse
+
+def app():
+    try:
+        print ("FIRMWARE "+Version+"\nStarting Services..." )
+        #iothub connect
+        print ("Connecting IoT Hub..." )
+        client = defs.iothub_client_init()
+        #provision device
+        print ("Provisioning Device on IoT Hub...\n.\n." )
+        defs.iothub_provision_device()
+        #Paralel methods
+        print ("Starting Listener method...\n.\n." )
+        device_method_thread = threading.Thread(target=defs.device_method_listener, args=(vars.client,))
+        device_method_thread.daemon = True
+        device_method_thread.start()
+        print ("Starting twin devices update method...\n.\n." )
+        twin_update_listener_thread = threading.Thread(target=defs.twin_update_listener, args=(vars.client,))
+        twin_update_listener_thread.daemon = True
+        twin_update_listener_thread.start()
+        ##Twin report patch
+        #reported_patch = {"Customer": "Microsoft"}
+        #vars.client.patch_twin_reported_properties(reported_patch)
+        if vars.registration_result.status == "assigned": print("Provision result: ", vars.registration_result.status)
+        while True:
+            defs.send_mqtt()
+    except KeyboardInterrupt:
+        print ( "App stopped")
+
+if __name__ == '__main__':
+    print ( "Starting App" )
+    print ( "Press Ctrl-C to exit" )
+    app()
