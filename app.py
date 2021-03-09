9d0f5960e89fa0ee093da2f49af7b3545eef7cd3
Pitágoras Bandeira <pithagoras@hotmail.com>
2021-03-09 14:22:51 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
+import os
+import sys
+#sys.path.append('C:/comgas/FIRMWARE/utils/lib')
+sys.path.append('/home/pi/pitagoras/devops/comgas/FIRMWARE/utils/lib')
 import vars
 ##Version 1.0
-vars.Version="3.0"
+vars.Version="1.0"
 
 import uuid
 import threading
-import os
-import sys
+
 import time
-#sys.path.append('C:/comgas/FIRMWARE/utils/lib')
-sys.path.append('/home/pi/pitagoras/devops/comgas/FIRMWARE/utils/lib')
+
 import defs
 from azure.iot.device import IoTHubDeviceClient, Message, MethodResponse
 
